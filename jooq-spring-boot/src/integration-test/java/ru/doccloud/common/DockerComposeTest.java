1c3e4736edc88ce10d3e885bbae72ce713c7b224
Oleg Zimakov <oleg@zimakov.cloud>
2020-05-17 22:57:32 +0300

    Using localhost for tests

--- jooq-spring-boot/src/integration-test/java/ru/doccloud/common/DockerComposeTest.java
+++ jooq-spring-boot/src/integration-test/java/ru/doccloud/common/DockerComposeTest.java
@@ -1,8 +1,10 @@
 package ru.doccloud.common;
 
 import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 import java.io.File;
+import java.time.Duration;
 
 public abstract class DockerComposeTest {
     public static DockerComposeContainer compose ;
@@ -11,8 +13,9 @@ public abstract class DockerComposeTest {
         compose = new DockerComposeContainer(
                 new File("src/integration-test/resources/docker-compose-test.yml"))
                 .withExposedService("hazelcast-test", 5701)
-                .withExposedService("hazelcast-man-test", 8080)
-                .withExposedService("docclouddb-test", 5432);
+                .withExposedService("docclouddb-test", 5432,
+                        Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(30))
+                );
         compose.start();
     }
 
